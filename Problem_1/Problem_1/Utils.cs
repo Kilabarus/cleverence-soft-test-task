d2b490fa10ef491f072effa6c6050bb94c480ba7
Roman Danilchenko <roman.danilchenko0139@gmail.com>
2025-06-19 22:38:32 +0300

    Add Decompressor and Utils classes

---
+++ Problem_1/Problem_1/Utils.cs
@@ -0,0 +1,10 @@
+﻿namespace Problem_1
+{
+    internal static class Utils
+    {
+        public static byte ConvertDigitCharToByte(char c)
+        {
+            return (byte)(c - '0');
+        }
+    }
+}
