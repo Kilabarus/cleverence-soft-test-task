3bea0a531113492240d3948c7c0dbe1051a1e0e7
Roman Danilchenko <roman.danilchenko0139@gmail.com>
2025-06-20 07:23:57 +0300

    Create project, add Server class

---
+++ Problem_2/Problem_2/Server.cs
@@ -0,0 +1,37 @@
+﻿namespace Problem_2
+{
+    internal static class Server
+    {
+        private static int count = 0;
+
+        private static readonly ReaderWriterLockSlim lockSlim = new();
+
+        public static int GetCount()
+        {
+            lockSlim.EnterReadLock();
+
+            try
+            {
+                return count;
+            }
+            finally
+            {
+                lockSlim.ExitReadLock();
+            }
+        }
+
+        public static void AddToCount(int value)
+        {
+            lockSlim.EnterWriteLock();
+
+            try
+            {
+                count += value;
+            }
+            finally
+            {
+                lockSlim.ExitWriteLock();
+            }
+        }
+    }
+}
